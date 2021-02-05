f575154ac38678926d638dd5c58b2981da3bebb6
astha3021 <47379203+astha3021@users.noreply.github.com>
2021-02-05 16:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
